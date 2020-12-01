09bb0fa701f4b2eeb7fd0025004f7d32cd75f350
bamaw <tbamaw@163.com>
2020-12-02 01:46:33 +0800

    springboot整合Security,三种实现方式|未关联数据库

---
+++ springboot-security/src/main/java/com/daobili/SpringbootSecurityApplication.java
@@ -0,0 +1,17 @@
+package com.daobili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bamaw
+ */
+
+@SpringBootApplication
+public class SpringbootSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurityApplication.class, args);
+    }
+
+}
